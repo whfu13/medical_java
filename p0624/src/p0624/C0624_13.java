f90683338c589370caf5f3133b89cbf02c3845f0
김정수 <whfu13@naver.com>
2024-06-24 20:45:02 +0900

    2024/06/24 Java

---
+++ p0624/src/p0624/C0624_13.java
@@ -0,0 +1,17 @@
+package p0624;
+
+import java.util.Scanner;
+
+public class C0624_13 {
+
+	public static void main(String[] args) {
+	      Scanner scan = new Scanner(System.in);
+	        int result = 0;
+	        for (int i =0; i<5; i++) {
+	            int num = scan.nextInt();
+	            result += num*num;
+	        }
+	        System.out.println(result%10);
+
+	    }
+	}
---
+++ p0624/src/p0624/C0624_14.java
@@ -0,0 +1,24 @@
+package p0624;
+
+import java.util.Scanner;
+
+public class C0624_14 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int[] arr = new int[30];
+		
+		for(int i=0;i<28;i++) {
+			int num = scan.nextInt();
+			arr[num-1] = 1;
+		}
+		
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]==0) {
+				System.out.println(i+1);
+			}
+		}
+	}
+
+}
